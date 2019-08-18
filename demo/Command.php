bb7ee9f089598cdc507bbeb15b33af0e46353705
Marijn Ophorst <marijn@monomelodies.nl>
2019-08-18 10:29:12 +0200

    add demo documentation and stuff

--- demo/Command.php
+++ demo/Command.php
@@ -6,22 +6,34 @@ use Monolyth\Cliff;
 
 /**
  * Demo command for Cliff.
+ *
+ * It showcases a required, optional and empty option and outputs the input.
  */
 class Command extends Cliff\Command
 {
     /**
      * @var string
+     *
+     * This option is required, as it doesn't have a default value.
      */
-    public $requiredProperty;
+    public $requiredOption;
     /**
      * @var string
+     *
+     * This option is optional, as can be seen by the default value.
      */
-    public $optionalProperty = 'dummy';
+    public $optionalOption = 'dummy';
     /**
      * @var bool
+     *
+     * This option is empty; it's a boolean (yes/no are our only options).
      */
-    public $emptyProperty = false;
+    public $emptyOption = false;
 
+    /**
+     * To run the demo command, exactly one argument needs to be passed. It can
+     * be any string value, it is simply outputted.
+     */
     public function __invoke(string $name)
     {
         fwrite(STDOUT, <<<EOT
@@ -29,20 +41,28 @@ Great! You called this command for `$name`.
 
 EOT
         );
-        fwrite(STDOUT, <<<EOT
-Our required property was set to `{$this->requiredProperty}`.
+        if (isset($this->requiredOption)) {
+            fwrite(STDOUT, <<<EOT
+Our required option was set to `{$this->requiredOption}`.
 
 EOT
-        );
-        if ($this->optionalProperty != 'dummy') {
+            );
+        } else {
             fwrite(STDOUT, <<<EOT
-Our optional property was overriden with `{$this->optionalProperty}`.
+The required option was not set.
 
 EOT
             );
         }
-        if ($this->emptyProperty) {
-            fwrite(STDOUT, "The empty property was also set.\n");
+        if ($this->optionalOption != 'dummy') {
+            fwrite(STDOUT, <<<EOT
+Our optional option was overriden with `{$this->optionalOption}`.
+
+EOT
+            );
+        }
+        if ($this->emptyOption) {
+            fwrite(STDOUT, "The empty option was also set.\n");
         }
     }
 }
