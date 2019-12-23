d99bc821645eb036a3b66b54ebcafb38f69a46da
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-23 09:38:37 +0100

    test forwarding

--- tests/command.php
+++ tests/command.php
@@ -1,16 +1,52 @@
 <?php
 
+namespace Monolyth\Cliff\Test;
+
+use Monolyth\Cliff\Command;
+use Generator;
+
+class FooCommand extends Command
+{
+    public function __invoke(string $forwardedCommand) : void
+    {
+    }
+}
+
+class BarCommand extends Command
+{
+    /** @var bool */
+    public $foo = false;
+
+    /** @var string */
+    public $bar;
+
+    /** @var bool */
+    private static $forwarded = false;
+
+    public function __invoke() : void
+    {
+        self::$forwarded = true;
+    }
+
+    public static function wasForwarded() : bool
+    {
+        return self::$forwarded;
+    }
+}
+
 use GetOpt\GetOpt;
 
 /** Testsuite for Cliff commands */
 return function () : Generator {
     /** We can instantiate a command with default (CLI) options. */
     yield function () : void {
-        $command = new class(['bar']) extends Monolyth\Cliff\Command {
+        $command = new class(['bar']) extends Command {
 
-            private $foo = '';
-
+            /** @var bool */
             public $bar = false;
+
+            /** @var string */
+            private $foo = '';
 
             public function __invoke(string $arg)
             {
@@ -31,11 +67,13 @@ return function () : Generator {
     /** We can instantiate a command with custom options. */
     yield function () : void {
         foreach (['--bar', '-b'] as $argument) {
-            $command = new class(['bar', $argument]) extends Monolyth\Cliff\Command {
+            $command = new class(['bar', $argument]) extends Command {
 
-                private $foo = '';
-
+                /** @var bool */
                 public $bar = false;
+
+                /** @var string */
+                private $foo = '';
 
                 public function __invoke(string $arg)
                 {
@@ -56,10 +94,17 @@ return function () : Generator {
 
     /** toPhpName correctly converts command names to PHP ones. */
     yield function () : void {
-        $name = Monolyth\Cliff\Command::toPhpName('foo:bar');
+        $name = Command::toPhpName('foo:bar');
         assert($name === 'Foo\Bar');
-        $name = Monolyth\Cliff\Command::toPhpName('foo/bar');
+        $name = Command::toPhpName('foo/bar');
         assert($name === 'Foo\Bar');
+    };
+
+    /** A command can forward to other commands, with the correct operands passed. */
+    yield function () : void {
+        $command = new FooCommand(['monolyth/cliff/test/bar-command', '--foo', '--bar=baz']);
+        $command->execute();
+        assert(BarCommand::wasForwarded() === true);
     };
 };
 
